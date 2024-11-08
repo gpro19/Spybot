359e5ef7f4fa18ef9b9a943fa57cf435d7e9983f
gpro19 <119834015+gpro19@users.noreply.github.com>
2024-11-08 13:07:15 +0700

    Update start.py

--- start.py
+++ start.py
@@ -68,3 +68,52 @@ def new_chat_members(update: Update, context: CallbackContext) -> None:
 
         # Kirim pesan ke grup admin atau grup tertentu
         context.bot.send_message(chat_id='-1002043233293', text=pesan, parse_mode='HTML')
+
+
+def send_donation_info(update: Update, context: CallbackContext) -> None:
+    # Pesan yang akan dikirim
+    donation_message = (
+        "<b>Yuk Support Bot Familiy 100 Dengan Cara Donasi</b>\n\n"
+        "Dukung Familiy 100 agar dapat selalu berkembang dan dapat membayar biaya server bot dengan cara donasi. "
+        "Sedikit bantuan dari anda sangat berharga.\n\n"
+        "Donasi bisa melalui :\n"
+        "|- <a href='https://telegra.ph/file/4c0b95048eb677ccfcafb.jpg'>klik untuk donasi via qris</a>\n"
+        "|- <a href='https://tiptap.id/@dutabotid'>klik untuk donasi via tiptap</a>\n"
+        "|- <a href='https://trakteer.id/DutabotID/tip'>klik untuk donasi via trakteer</a>\n\n"
+        "Contact : @Mazekubot"
+    )
+
+    # Mengirim pesan ke pengguna
+    update.message.reply_html(donation_message)
+
+
+def send_game_rules(update: Update, context: CallbackContext) -> None:
+    # Pesan aturan permainan yang akan dikirim
+    rules_message = (
+        "Aturannya sangat sederhana, Cukup menjawab pertanyaan dengan benar. "
+        "Jawaban yang benar akan mendapatkan (1 - 10) poin. Selamat bermain."
+    )
+
+    # Mengirim pesan aturan ke pengguna dengan opsi untuk melindungi konten
+    update.message.reply_html(rules_message, protect_content=True)
+
+
+
+def send_help_info(update: Update, context: CallbackContext) -> None:
+    # Pesan bantuan yang akan dikirim
+    help_message = (
+        "<b>Berikut adalah perintah yang tersedia:</b>\n\n"
+        "/play : mulai game\n"
+        "/nyerah : menyerah dari game\n"
+        "/next : Pertanyaan berikutnya\n"
+        "/help : membuka pesan bantuan\n"
+        "/stats : melihat statistik kamu\n"
+        "/top : lihat top skor global\n"
+        "/topgrup : lihat top global grup\n"
+        "/peraturan : aturan bermain\n"
+        "/donasi : dukung bot ini agar tetap aktif"
+    )
+
+    # Mengirim pesan bantuan ke pengguna
+    update.message.reply_html(help_message)
+
